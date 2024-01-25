719904be349890353df4bd641243803c6608c595
spacedustz <tensorflow555@gmail.com>
2024-01-26 00:21:45 +0900

    App에 Router 연결

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import {RootContainer} from "./styles/container/RootContainer.tsx";
+import Navigation from "./styles/navigation/Navigation.tsx";
+import Router from "./router/Router.tsx";
+import ProfileCard from "./styles/card/ProfileCard.tsx";
 
 const App: React.FC = () => {
   return (
     <RootContainer>
-
+      <Navigation />
+        <ProfileCard />
+        <Router />
     </RootContainer>
   )
 }
