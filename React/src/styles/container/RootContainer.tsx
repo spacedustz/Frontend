031f05298af155164ca0cf92a835df8fd0386e7b
spacedustz <tensorflow555@gmail.com>
2024-01-25 23:23:28 +0900

    Root Container 추가

---
+++ React/src/styles/container/RootContainer.tsx
@@ -0,0 +1,13 @@
+// import {Container} from "react-bootstrap";
+import styled from "styled-components";
+
+export const RootContainer = styled.div`
+    font-family: Arial, sans-serif;
+    font-size: 15px;
+    font-weight: bold;
+    background-image: url("../../../public/assets/background/night.jpg");
+    background-size: 100%;
+    width: 100vw;
+    height: 100vh;
+    text-align: center;
+`;
