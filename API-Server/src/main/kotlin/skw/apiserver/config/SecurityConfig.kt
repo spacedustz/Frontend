06e0d4602a3983d6e6e7eb796fc7fc577622c7ee
spacedustz <tensorflow555@gmail.com>
2024-01-26 13:18:46 +0900

    Security 허용 URL 변경

---
+++ API-Server/src/main/kotlin/skw/apiserver/config/SecurityConfig.kt
@@ -0,0 +1,55 @@
+package skw.apiserver.config
+
+import lombok.RequiredArgsConstructor
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import skw.apiserver.filter.JwtAuthenticationFilter
+
+/**
+ * @author 신건우
+ * @desc 쿠키, 세션 사용 X, CSRF Disable
+ */
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+class SecurityConfig(
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter
+) {
+//    private val defaultPages = arrayOf(
+//        "/",
+//        "/intro",
+//        "/skills",
+//        "/careers",
+//        "/licenses",
+//        "/educations"
+//    )
+//
+    private val signupPages = arrayOf(
+        "/",
+        "/sign-up",
+        "/sign-in",
+    )
+
+    @Bean
+    fun filterChain(http: HttpSecurity) = http
+        .csrf(CsrfConfigurer<HttpSecurity>::disable)
+        .headers { header -> header.frameOptions(HeadersConfigurer<HttpSecurity>.FrameOptionsConfig::sameOrigin) }
+        .authorizeHttpRequests { authorize ->
+            authorize
+                .requestMatchers(*signupPages).permitAll()
+                .anyRequest().authenticated()
+        }
+        .sessionManagement { session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+        .addFilterBefore(jwtAuthenticationFilter, BasicAuthenticationFilter::class.java)
+        .build()!!
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+}
