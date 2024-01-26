dafff6f4760a7c5582c8b0f66a38c44c0e33f9e1
owen <wavefunctionc@gmail.com>
2024-01-26 10:44:58 +0900

    Root Container Style 변경

--- React/src/styles/container/RootContainer.tsx
+++ React/src/styles/container/RootContainer.tsx
@@ -6,8 +6,9 @@ export const RootContainer = styled.div`
     font-size: 15px;
     font-weight: bold;
     background-image: url("../../../public/assets/background/night.jpg");
-    background-size: 100%;
+    background-size: cover;
+    background-position: center center;
+    min-height: 100vh;
     width: 100vw;
-    height: 100vh;
     text-align: center;
 `;
