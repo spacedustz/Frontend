fc4d5565b849b89ca9f16dd6d89d024bbaf3b5a1
owen <wavefunctionc@gmail.com>
2024-01-26 13:18:13 +0900

    Backend Server 추가

---
+++ API-Server/build.gradle.kts
@@ -0,0 +1,69 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.2.1"
+	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("jvm") version "1.9.21"
+	kotlin("plugin.spring") version "1.9.21"
+	kotlin("plugin.jpa") version "1.9.21"
+}
+
+group = "skw"
+version = "1.0"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	// Web
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// JPA
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+	// Security
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	// WebSocket
+	implementation("org.springframework.boot:spring-boot-starter-websocket")
+
+	// Webflux
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// JWT
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+	// MariaDB
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
+	// Lombok
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ API-Server/src/test/kotlin/skw/apiserver/ApiServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package skw.apiserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
