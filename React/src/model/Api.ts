7ac704e8683e412e1515d7b6fca587a203662a07
owen <wavefunctionc@gmail.com>
2024-01-28 03:39:08 +0900

    Request API 로직 변경

--- React/src/model/Api.ts
+++ React/src/model/Api.ts
@@ -6,13 +6,7 @@ export interface User {
 }
 
 const local = 'http://localhost:8080'
-const server = 'http://43.202.203.180:8080'
-
-export const RequestCommentApi = axios.get(local + '/api/comment', {
-    headers: {
-        Authorization: `Bearer ${localStorage.getItem('jwt')}`,
-    },
-});
+// const server = 'http://43.202.203.180:8080'
 
 export const requestSignUp = async (user: User) => {
     return await axios.post(local + '/api/user/sign-up', user);
@@ -20,4 +14,19 @@ export const requestSignUp = async (user: User) => {
 
 export const requestSignIn = async (user: User) => {
     return await axios.post(local + '/api/user/sign-in', user);
+}
+
+export const postComment = async (description: string) => {
+    const token = localStorage.getItem('jwt')
+    const userName = localStorage.getItem('username')
+
+    return await axios.post(
+        local + '/api/comment/post',
+        { userName,  description },
+        { headers : { Authorization: `Bearer ${token}`}}
+    );
+}
+
+export const getAllComments = async () => {
+    return await axios.get(local + '/api/comment')
 }
