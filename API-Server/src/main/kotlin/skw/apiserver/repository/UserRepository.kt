edc8556ad9a1f9418fa2f144306d1921d4edeb3d
owen <wavefunctionc@gmail.com>
2024-01-27 16:43:57 +0900

    findByName Optional

--- API-Server/src/main/kotlin/skw/apiserver/repository/UserRepository.kt
+++ API-Server/src/main/kotlin/skw/apiserver/repository/UserRepository.kt
@@ -3,8 +3,9 @@ package skw.apiserver.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import skw.apiserver.entity.User
 import skw.apiserver.enum.UserType
+import java.util.*
 
 interface UserRepository : JpaRepository<User, Long> {
-    fun findByName(name: String): User?
+    fun findByName(name: String): Optional<User?>
     fun findAllByType(type: UserType): List<User>
 }
