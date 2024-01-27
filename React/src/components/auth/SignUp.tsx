9fa1bd852444e50dbe7c2ce76135bd87891a48cd
spacedustz <tensorflow555@gmail.com>
2024-01-27 18:59:53 +0900

    회원가입 세부적인 Modal 창 띄우기 기능 추가 & 중복 이름 검증 추가

--- React/src/components/auth/SignUp.tsx
+++ React/src/components/auth/SignUp.tsx
@@ -7,12 +7,17 @@ const SignUpContainer = styled.div`
     margin: 10;
 `;
 
-
 const SignUp: React.FC = () => {
     const [show, setShow] = useState<boolean>(false);
-    const [user, setUser] = useState<User>({id: 0, name: '', password: '', type: ''});
+    const [showSuccess, setShowSuccess] = useState<boolean>(false);
+    const [showError, setShowError] = useState<boolean>(false);
+    const [user, setUser] = useState<User>({ name: '', password: '' });
 
-    const handleClose = (): void => setShow(false);
+    const handleClose = (): void => {
+        setShow(false);
+        setShowSuccess(false);
+        setShowError(false);
+    };
     const handleShow = (): void => setShow(true);
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
@@ -25,15 +30,17 @@ const SignUp: React.FC = () => {
     const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
         e.preventDefault();
         try {
-            const response = await requestSignUp(user);
-            if (response.status === 200) {
-                localStorage.setItem('jwt', response.data.token);
-                handleClose();
-            } else {
-                alert('이미 사용중인 아이디입니다.');
-            }
+            await requestSignUp(user);
+            setUser({ name: '', password: '' }); // 초기화
+            setShow(false)
+            setShowSuccess(true);
         } catch (error) {
             console.error(error);
+            if (error.response.status === 400) {
+                setUser({name: '', password: ''})
+                setShow(false)
+                setShowError(true);
+            }
         }
     };
 
@@ -66,8 +73,27 @@ const SignUp: React.FC = () => {
                     </Form>
                 </Modal.Body>
             </Modal>
+
+            <Modal show={showSuccess} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>회원가입 성공</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {user.name}님, 회원가입을 축하합니다!
+                </Modal.Body>
+            </Modal>
+
+            <Modal show={showError} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>회원가입 실패</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    이미 존재하는 이름입니다.
+                </Modal.Body>
+            </Modal>
         </SignUpContainer>
     );
 };
 
 export default SignUp;
+
