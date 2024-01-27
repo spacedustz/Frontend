a18754be10d628ca1e21690f2e3bf2fea53ef6e0
owen <wavefunctionc@gmail.com>
2024-01-27 16:44:24 +0900

    Global Error Handler

---
+++ API-Server/src/main/kotlin/skw/apiserver/error/GlobalErrorHandler.kt
@@ -0,0 +1,31 @@
+package skw.apiserver.error
+
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+open class CommonException(
+    message: String,
+    val status: HttpStatus
+) : RuntimeException(message)
+
+class CommonExceptionResponse(
+    val code: Int,
+    val message: String
+)
+
+@RestControllerAdvice
+class GlobalErrorHandler {
+    @ExceptionHandler(CommonException::class)
+    fun commonExceptionHandler(e: CommonException): ResponseEntity<CommonExceptionResponse> {
+        val error = CommonExceptionResponse(code = e.status.value(), message = e.message ?: "")
+        return ResponseEntity(error, e.status)
+    }
+
+    companion object {
+        private val log: Logger = LogManager.getLogger(GlobalErrorHandler::class.java)
+    }
+}
