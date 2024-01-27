8abbdd481379dcd9de1202533929c26ff677af4e
owen <wavefunctionc@gmail.com>
2024-01-27 16:43:44 +0900

    UserService 예외 처리

--- API-Server/src/main/kotlin/skw/apiserver/service/UserService.kt
+++ API-Server/src/main/kotlin/skw/apiserver/service/UserService.kt
@@ -7,12 +7,14 @@ import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import org.hibernate.boot.model.naming.IllegalIdentifierException
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.HttpStatus
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import skw.apiserver.dto.*
 import skw.apiserver.entity.User
 import skw.apiserver.enum.UserType
+import skw.apiserver.error.CommonException
 import skw.apiserver.repository.UserRepository
 import skw.apiserver.token.TokenProvider
 
@@ -40,25 +42,36 @@ class UserService(
         try {
             userRepository.save(User.createOf(request, encoder))
         } catch (e: Exception) {
-            log.error("이미 사용중인 아이디입니다.")
-            throw IllegalArgumentException("이미 사용중인 아이디입니다.")
+            log.error("이미 존재하는 이름입니다.")
+            throw CommonException("이미 존재하는 이름입니다", HttpStatus.BAD_REQUEST)
         }
     )
 
     fun signIn(request: SignInRequest): SignInResponse {
         val user = userRepository.findByName(request.name)
-            ?.takeIf { encoder.matches(request.password, it.password) }
-            ?: throw IllegalIdentifierException("아이디 또는 비밀번호가 일치하지 않습니다.")
+            .filter { encoder.matches(request.password, it?.password) }
+            .orElseThrow { CommonException("아이디 또는 비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST) }
 
-        val token = tokenProvider.createToken("${user.id}:${user.type}")
+        val token = tokenProvider.createToken("${user!!.id}:${user.type}")
 
         return SignInResponse(user.name, user.type, token)
+    }
+
+    @Transactional(readOnly = true)
+    fun checkDuplicateUserName(name: String): Boolean {
+        try {
+            val optUser = userRepository.findByName(name)
+            return optUser.isPresent
+        } catch (e: Exception) {
+            log.error("이미 존재하는 이름입니다.")
+            throw CommonException("이미 존재하는 이름입니다.", HttpStatus.BAD_REQUEST)
+        }
     }
 
     fun updatePassword(id: Long, request: UpdateRequest): UpdatePasswordResponse {
         val user = userRepository.findByIdOrNull(id)
             ?.takeIf { encoder.matches(request.password, it.password) }
-            ?: throw IllegalArgumentException("아이디 또는 비밀번호가 일치하지 않습니다.")
+            ?: throw CommonException("아이디 또는 비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST)
 
         user.changePassword(request, encoder)
 
