a4b5579b3ffaec50a27cb2a5d25ba82d5a9eb5d3
owen <wavefunctionc@gmail.com>
2024-01-28 03:43:06 +0900

    WebSocket Handler 추가

---
+++ API-Server/src/main/kotlin/skw/apiserver/config/CommentWebSocketHandler.kt
@@ -0,0 +1,26 @@
+package skw.apiserver.config
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.springframework.web.socket.CloseStatus
+import java.util.concurrent.ConcurrentHashMap
+import org.springframework.web.socket.WebSocketSession
+import org.springframework.web.socket.handler.TextWebSocketHandler
+
+class CommentWebSocketHandler : TextWebSocketHandler() {
+    companion object {
+        val sessionMap: MutableMap<String, WebSocketSession> = ConcurrentHashMap()
+        private val log: Logger = LogManager.getLogger(this::class.java.name)
+    }
+
+    @Throws(Exception::class)
+    override fun afterConnectionEstablished(session: WebSocketSession) {
+        sessionMap[session.id] = session
+        log.info("Comment Socket 연결 - Session ID : ${session.id}")
+    }
+
+    @Throws(Exception::class)
+    override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
+        sessionMap.remove(session.id)
+        log.info("Comment Socket 종료 - Session ID : ${session.id}")
+    }
+}
