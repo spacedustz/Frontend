2b308334905103df4b392dfd89fa3fe8342ea801
owen <wavefunctionc@gmail.com>
2024-01-28 06:08:34 +0900

    WebSocket Stomp로 변경

--- API-Server/src/main/kotlin/skw/apiserver/config/WebSocketConfig.kt
+++ API-Server/src/main/kotlin/skw/apiserver/config/WebSocketConfig.kt
@@ -1,14 +1,19 @@
 package skw.apiserver.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.socket.config.annotation.*
 
 @Configuration
-@EnableWebSocket
-class WebSocketConfig: WebSocketConfigurer {
-    override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
-        registry
-            .addHandler(CommentWebSocketHandler(), "/api/comment/list")
+@EnableWebSocketMessageBroker
+class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+    override fun configureMessageBroker(config: MessageBrokerRegistry) {
+        config.enableSimpleBroker("/api/comment")
+        config.setApplicationDestinationPrefixes("/")
+    }
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/ws")
             .setAllowedOrigins("*")
     }
 }
