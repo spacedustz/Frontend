f998286e7feed8d4a1ecc6d20f11e738728cf478
spacedustz <spacedustw@gmail.com>
2024-01-27 16:44:41 +0900

    API Request URI 변경

--- React/src/model/Api.ts
+++ React/src/model/Api.ts
@@ -1,20 +1,22 @@
 import axios from "axios";
 
 export interface User {
+    id?: number
     name: string
     password: string
+    type: string
 }
 
-export const RequestCommentApi = axios.get('http://localhost:8080/comments', {
+export const RequestCommentApi = axios.get('http://43.202.203.180:8080/api/comment', {
     headers: {
         Authorization: `Bearer ${localStorage.getItem('jwt')}`,
     },
 });
 
 export const requestSignUp = async (user: User) => {
-    return await axios.post('http://localhost:8080/sign-up', user);
+    return await axios.post('http://43.202.203.180:8080/api/user/sign-up', user);
 }
 
 export const requestSignIn = async (user: User) => {
-    return await axios.post('http://localhost:8080/sign-in', user);
+    return await axios.post('http://43.202.203.180:8080/api/user/sign-in', user);
 }
