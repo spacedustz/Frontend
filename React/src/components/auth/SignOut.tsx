9e39face5d6d962d71ce12700e493ecaf9d156e2
owen <wavefunctionc@gmail.com>
2024-01-28 03:45:35 +0900

    LocalStorage에 username 추가

--- React/src/components/auth/SignOut.tsx
+++ React/src/components/auth/SignOut.tsx
@@ -3,7 +3,8 @@ import {Button} from "react-bootstrap";
 
 const SignOut: React.FC = () => {
     const handleClick = (): void => {
-        localStorage.removeItem('jwt'); // 토큰 삭제
+        localStorage.removeItem('jwt');
+        localStorage.removeItem('username')
         window.location.reload(); // 페이지 새로고침으로 상태 초기화
         console.log('로그아웃 - LocalStorage JWT Token 제거 완료')
     };
