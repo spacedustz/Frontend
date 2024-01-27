a79cd25a9b2eb25e39bbadcc578fcd34001aad98
spacedustz <tensorflow555@gmail.com>
2024-01-27 19:00:25 +0900

    Add WebSocket Config

---
+++ API-Server/src/main/kotlin/skw/apiserver/config/WebSocketConfig.kt
@@ -0,0 +1,23 @@
+package skw.apiserver.config
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        registry.enableSimpleBroker("/")
+        registry.setApplicationDestinationPrefixes("/")
+    }
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/ws")
+            .setAllowedOrigins("*")
+            .addInterceptors(HttpSessionHandshakeInterceptor())
+    }
+}
