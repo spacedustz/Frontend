4c7295c118e3a367a4aa6edb093e7d5df0964476
spacedustz <tensorflow555@gmail.com>
2024-01-27 16:45:17 +0900

    Comment App Component 로직 수정

--- React/src/components/Comment/CommentApp.tsx
+++ React/src/components/Comment/CommentApp.tsx
@@ -1,42 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import CommentForm from "./CommentForm.tsx";
-import CommentList from "./CommentList.tsx";
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
-import {Comment} from "../../model/Comment.ts";
+import CommentForm from './CommentForm';
+import CommentList from './CommentList';
 
 const CommentApp: React.FC = () => {
     const [comments, setComments] = useState<Comment[]>([]);
+    const webSocketRef = useRef<WebSocket | null>(null); // 추가
 
     useEffect(() => {
-        // Fetch comments from the backend every 3 seconds
-        const fetchComments = async () => {
-            try {
-                const response = await axios.get<Comment[]>('http://13.125.251.209:8080/api/comments');
-                // const response = await axios.get<Comment[]>('http://localhost:8080/api/comments');
-                setComments(response.data);
-            } catch (error) {
-                console.error('Error fetching comments:', error);
-            }
+        webSocketRef.current = new WebSocket('ws://13.125.251.209:8080/comment');
+
+        webSocketRef.current.onopen = () => {
+            console.log('WebSocket 연결 완료');
         };
 
-        const intervalId = setInterval(fetchComments, 500);
+        webSocketRef.current.onmessage = (message) => {
+            const dataFromServer = JSON.parse(message.data);
+            setComments(dataFromServer);
+        };
 
-        // Cleanup interval on component unmount
-        return () => clearInterval(intervalId);
+        webSocketRef.current.onerror = (error) => {
+            console.error(`WebSocket 오류: ${error}`);
+        };
+
+        return () => {
+            if (webSocketRef.current) {
+                webSocketRef.current.close();
+            }
+        };
     }, []);
 
     const handleAddComment = async (comment: string) => {
         try {
-            // Send the new comment to the backend
             const response = await axios.post('http://13.125.251.209:8080/api/comments', { comment });
-            // const response = await axios.post('http://localhost:8080/api/comments', { comment: comment });
-
             const newComment = response.data;
-
-            // Update the local state with the new comment
-            setComments([...comments, newComment]);
+            setComments((prevComments) => [...prevComments, newComment]);
         } catch (error) {
-            console.error('Error adding comment:', error);
+            console.error('댓글 추가 오류:', error);
         }
     };
 
