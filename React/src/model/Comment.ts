fbbff0dcc1b161b597ec764ba9505271abea323a
spacedustz <tensorflow555@gmail.com>
2024-01-28 03:40:06 +0900

    Comment Type 필드 추가

--- React/src/model/Comment.ts
+++ React/src/model/Comment.ts
@@ -1,8 +1,27 @@
-export interface Comment {
-    id: number
-    comment: string
-    createdAt: string
+import React, {createContext} from "react";
+
+export type User = {
+    id: number,
+    name: string,
+    password: string,
+    type: string,
+    createdAt: string,
+    modifiedAt: string
 }
+
+export type Comment = {
+    id: number,
+    description: string,
+    createdAt: string,
+    user: User
+}
+
+type CommentContextType = {
+    comments: Comment[],
+    setComments: React.Dispatch<React.SetStateAction<Comment[]>>,
+};
+
+export const CommentContext = createContext<CommentContextType | undefined>(undefined);
 
 export interface CommentListProps {
     comments: Comment[];
