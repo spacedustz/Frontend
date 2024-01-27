66473f70dbbd7f05337c5a245fa21de7c87d101d
owen <wavefunctionc@gmail.com>
2024-01-28 03:44:46 +0900

    UserDTO - companion object 추가

--- API-Server/src/main/kotlin/skw/apiserver/dto/SecurityRequests.kt
+++ API-Server/src/main/kotlin/skw/apiserver/dto/SecurityRequests.kt
@@ -1,9 +1,18 @@
 package skw.apiserver.dto
 
+import skw.apiserver.entity.User
+
 data class SignUpRequest(
     val name: String,
     var password: String
-)
+) {
+    companion object {
+        fun createOf(name: String, password: String) = SignUpRequest(
+            name = name,
+            password = password
+        )
+    }
+}
 
 data class SignInRequest(
     val name: String,
