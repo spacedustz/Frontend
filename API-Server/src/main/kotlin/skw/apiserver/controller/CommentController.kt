56d7f941c1b8229ae10c8747e8aa9568567e2cc2
owen <wavefunctionc@gmail.com>
2024-01-28 03:40:38 +0900

    Comment Controller 추가

---
+++ API-Server/src/main/kotlin/skw/apiserver/controller/CommentController.kt
@@ -0,0 +1,40 @@
+package skw.apiserver.controller
+
+import lombok.RequiredArgsConstructor
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+
+import org.springframework.web.bind.annotation.*
+import skw.apiserver.dto.DeleteCommentDto
+import skw.apiserver.dto.PostCommentDto
+import skw.apiserver.dto.UpdateCommentDto
+import skw.apiserver.entity.Comment
+import skw.apiserver.service.CommentService
+
+@RestController
+@RequestMapping("/api/comment")
+@RequiredArgsConstructor
+class CommentController(
+    private val commentService: CommentService
+) {
+    @PostMapping("/post")
+    fun writeComment(@RequestBody request: PostCommentDto): ResponseEntity<Comment> {
+        return ResponseEntity(commentService.writeComment(request.userName, request.description), HttpStatus.CREATED)
+    }
+
+    @PatchMapping("/update/{commentId}")
+    fun updateComment(@PathVariable commentId: Long, @RequestBody request: UpdateCommentDto): ResponseEntity<Comment> {
+        return ResponseEntity(commentService.updateComment(commentId, request.newDescription, request.password), HttpStatus.OK)
+
+    }
+
+    @DeleteMapping("/delete/{commentId}")
+    fun deleteComment(@PathVariable commentId: Long, @RequestBody request: DeleteCommentDto): ResponseEntity<Unit> {
+        return ResponseEntity(commentService.deleteComment(commentId, request.password), HttpStatus.OK)
+    }
+
+    @GetMapping
+    fun getAllComments() {
+        commentService.getAllComments()
+    }
+}
