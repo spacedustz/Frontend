75a3ed2fac9fbc53e081863272125ce5310e7d94
owen <wavefunctionc@gmail.com>
2024-01-30 15:55:31 +0900

    Vite 환경변수 설정

--- React/src/vite-env.d.ts
+++ React/src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    env: {
+        BASE_URL?: string,
+        WEB_SOCKET_URL?: string,
+    }
+}
+
+interface ImportMeta {
+    env: ImportMetaEnv
+}
