21533b94b550c5fc45c10783d5a421f409bb8a82
spacedustz <tensorflow555@gmail.com>
2024-01-31 14:21:07 +0900

    ProfileCard Components 모듈로 이동

---
+++ React/src/components/ProfileCard.tsx
@@ -0,0 +1,20 @@
+import Card from 'react-bootstrap/Card';
+import React from "react";
+import CardContainer, {CardTitle, MainCard} from "../styles/container/CardContainer.tsx";
+
+const ProfileCard: React.FC = () => {
+    return (
+        <CardContainer>
+            <MainCard style={{width: '20rem'}}>
+                <Card.Img variant="top" src="https://techstack-generator.vercel.app/react-icon.svg"/>
+                <Card.Body>
+                    <CardTitle>😺 신건우 😺</CardTitle><br/>
+                    <strong>Frontend & React</strong><br/>
+                    <strong>학습 기록 사이트</strong>
+                </Card.Body>
+            </MainCard>
+        </CardContainer>
+    );
+}
+
+export default ProfileCard;
