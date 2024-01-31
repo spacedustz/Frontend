aa83e4abb0ccf0b9fcc1be78e273dddcf0beddb5
spacedustz <tensorflow555@gmail.com>
2024-01-31 10:42:33 +0900

    불필요 import 제거

--- API-Server/src/main/kotlin/skw/apiserver/service/UserService.kt
+++ API-Server/src/main/kotlin/skw/apiserver/service/UserService.kt
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor
 import lombok.extern.slf4j.Slf4j
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
-import org.hibernate.boot.model.naming.IllegalIdentifierException
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
 import org.springframework.security.crypto.password.PasswordEncoder
