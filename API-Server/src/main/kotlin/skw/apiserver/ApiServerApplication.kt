c4e76348188db31ee34fbabfe03105837eb2a9bb
spacedustz <tensorflow555@gmail.com>
2024-01-31 10:42:12 +0900

    Scheduling 추가

--- API-Server/src/main/kotlin/skw/apiserver/ApiServerApplication.kt
+++ API-Server/src/main/kotlin/skw/apiserver/ApiServerApplication.kt
@@ -2,7 +2,9 @@ package skw.apiserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 class ApiServerApplication
 
