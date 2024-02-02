51ad0c9f5762553e9cf6fcbcb621eb16f75dd82d
spacedustz <tensorflow555@gmail.com>
2024-02-02 19:28:14 +0900

    댓글 상위 컴포넌트로 로직 이동

--- React/src/components/Comment/CommentApp.tsx
+++ React/src/components/Comment/CommentApp.tsx
@@ -1,12 +1,160 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import CommentForm from './CommentForm';
 import CommentList from './CommentList';
+import {deleteComment, getAllComments, modifyComment} from "../../model/Api.ts";
+import {Client, Frame} from "@stomp/stompjs";
+import {Comment} from "../../model/Comment.ts";
 
 const CommentApp: React.FC = () => {
-    return(
+    const [newComment, setNewComment] = useState<string>('');
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+    const [editingCommentId, setEditingCommentId] = useState<number | null>(null);
+    const [comments, setComments] = useState<Comment[]>([]);
+    const stompClientRef = useRef<Client | null>(null);
+
+    /* WebSocket */
+    const connectWebSocket = () => {
+        if (stompClientRef.current && stompClientRef.current.connected) {
+            return;
+        }
+
+        const stompClient = new Client({
+            brokerURL: import.meta.env.VITE_SOCKET_URL,
+            reconnectDelay: 5000,
+            heartbeatIncoming: 4000,
+            heartbeatOutgoing: 4000,
+        });
+
+        stompClient.onConnect = function () {
+            stompClientRef.current = stompClient;
+
+            stompClient.subscribe("/api/comment/list", (frame: Frame) => {
+                if (frame.body) {
+                    const receivedComments: Comment[] = JSON.parse(frame.body);
+                    setComments(receivedComments);
+                }
+            });
+
+            console.log("WebSocket 세션이 [생성] 되었습니다.");
+        };
+
+        stompClient.activate();
+    };
+
+    const fetchComments = async () => {
+        const result = await getAllComments();
+        if (result.data && Array.isArray(result.data)) {
+            setComments(result.data);
+        }
+    };
+
+    const init = async (): Promise<void> => {
+        connectWebSocket();
+        await fetchComments();
+    };
+
+    useEffect(() => {
+        init().catch(error => console.error("WebSocket 연결 실패 : ", error))
+
+        return () => {
+            if (stompClientRef.current) {
+                stompClientRef.current?.deactivate();
+                console.log("WebSocket 세션이 [종료] 되었습니다.");
+            }
+        };
+    }, []);
+
+    /* 댓글 수정 & 삭제 */
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNewComment(event.target.value);
+    };
+
+    const handleEditComment = async () => {
+        if (isEditing && editingCommentId !== null) {
+            const jwt = localStorage.getItem('jwt');
+            if (!jwt) {
+                alert('비밀번호가 저장되지 않았습니다.');
+                return;
+            }
+
+            const requestData = {
+                commentId: editingCommentId,
+                newDescription: newComment,
+                jwt
+            };
+
+            try {
+                const response = await modifyComment(requestData);
+                if (response.status === 200) {
+                    alert('댓글이 수정되었습니다.');
+                    console.log('댓글 수정 완료')
+
+                    setIsEditing(false);
+                    setNewComment('');
+                    setEditingCommentId(null);
+
+                    const updatedComments = await getAllComments();
+
+                    if (updatedComments.data && Array.isArray(updatedComments.data)) {
+                        setComments(updatedComments.data);
+                    }
+                } else {
+                    alert('비밀번호가 틀립니다.');
+                }
+            } catch (error) {
+                console.error('댓글 수정 에러:', error);
+            }
+        }
+    };
+
+    const handleDeleteComment = async (commentId: number) => {
+        const jwt = localStorage.getItem('jwt');
+        if (!jwt) {
+            alert('비밀번호가 저장되지 않았습니다.');
+            return;
+        }
+
+        const requestData = {
+            commentId,
+            jwt
+        };
+
+        try {
+            const response = await deleteComment(requestData);
+            if (response.status === 200) {
+                console.log('댓글 삭제 완료')
+                alert('댓글이 삭제되었습니다.');
+
+                const updatedComments = await getAllComments();
+
+                if (updatedComments.data && Array.isArray(updatedComments.data)) {
+                    setComments(updatedComments.data);
+                }
+            } else {
+                alert('비밀번호가 틀립니다.');
+            }
+        } catch (error) {
+            console.error('댓글 삭제 에러:', error);
+        }
+    };
+
+    return (
         <div>
-            <CommentForm />
-            <CommentList />
+            <CommentForm
+                newComment={newComment}
+                onInputChange={handleInputChange}
+                onSubmitComment={isEditing ? handleEditComment : handleEditComment}
+                isEditing={isEditing}
+            />
+            <CommentList
+                comments={comments}
+                onEditComment={(commentId, comment) => {
+                    setIsEditing(true);
+                    setEditingCommentId(commentId);
+                    setNewComment(comment);
+                }}
+                onDeleteComment={handleDeleteComment}
+            />
         </div>
     );
 };
--- React/src/components/Comment/CommentList.tsx
+++ React/src/components/Comment/CommentList.tsx
@@ -1,8 +1,6 @@
-import React, {useState, useEffect, useRef} from 'react';
-import {Client, Frame} from '@stomp/stompjs';
+import React, {useState} from 'react';
 import Pagination from "./Pagination.tsx";
-import {Comment} from "../../model/Comment.ts";
-import {getAllComments} from "../../model/Api.ts";
+import {Comment, CommentListProps} from "../../model/Comment.ts";
 import {
     CommentActions,
     CommentAuthor,
@@ -13,70 +11,12 @@ import {
     CommentUserType, DeleteButton, EditButton
 } from "../../styles/comment/List.ts";
 
-const CommentList: React.FC = () => {
-    const [comments, setComments] = useState<Comment[]>([]);
+const CommentList: React.FC<CommentListProps> = ({ comments, onEditComment, onDeleteComment }) => {
     const [currentPage, setCurrentPage] = useState(1);
-    const stompClientRef = useRef<Client | null>(null);
     const itemsPerPage = 7;
-
-    const connectWebSocket = () => {
-        if (stompClientRef.current && stompClientRef.current.connected) {
-            return; // 이미 연결된 경우 종료
-        }
-
-        const stompClient = new Client({
-            brokerURL: import.meta.env.VITE_SOCKET_URL,
-            // debug: function (str) {
-            //     console.log('WebSocket 연결 중 ... : ' + str);
-            // },
-            reconnectDelay: 5000,
-            heartbeatIncoming: 4000,
-            heartbeatOutgoing: 4000,
-        });
-
-        stompClient.onConnect = function () {
-            stompClientRef.current = stompClient;
-
-            stompClient.subscribe("/api/comment/list", (frame: Frame) => {
-                if (frame.body) {
-                    const receivedComments: Comment[] = JSON.parse(frame.body);
-                    setComments(receivedComments);
-                }
-            });
-
-            console.log("WebSocket 세션이 [생성] 되었습니다.");
-        };
-
-        stompClient.activate();
-    };
-
-    const fetchComments = async () => {
-        const result = await getAllComments();
-        if (result.data && Array.isArray(result.data)) {
-            setComments(result.data);
-        }
-    };
-
-    const init = async (): Promise<void> => {
-        connectWebSocket();
-        await fetchComments();
-    };
-
-    useEffect(() => {
-        init().catch(error => console.error("WebSocket 연결 실패 : ", error))
-
-        return () => {
-            if (stompClientRef.current) {
-                stompClientRef.current?.deactivate();
-                console.log("WebSocket 세션이 [종료] 되었습니다.");
-            }
-        };
-    }, []);
-
-    const lastItemIdx = currentPage * itemsPerPage; // 현재 페이지 * 페이지당 댓글 개수 1 x 10
-    const firstItemIdx = lastItemIdx - itemsPerPage; // 10 - 10
+    const lastItemIdx = currentPage * itemsPerPage;
+    const firstItemIdx = lastItemIdx - itemsPerPage;
     const currentComments: Comment[] = comments.slice(firstItemIdx, lastItemIdx);
-
     const paginate = (pageNum: number) => setCurrentPage(pageNum);
 
     return (
@@ -89,11 +29,13 @@ const CommentList: React.FC = () => {
                             <CommentUserType $userType={comment.userType}>{comment.userType}</CommentUserType>
                             <CommentTime>{comment.createdAt}</CommentTime>
                         </CommentAuthorAndTime>
+
                         <CommentActions>
-                            <EditButton>수정</EditButton>
-                            <DeleteButton>삭제</DeleteButton>
+                            <EditButton onClick={() => onEditComment(comment.commentId, comment.description)}>수정</EditButton>
+                            <DeleteButton onClick={() => onDeleteComment(comment.commentId)}>삭제</DeleteButton>
                         </CommentActions>
                     </CommentTop>
+
                     <CommentDescription>{comment.description}</CommentDescription>
                 </CommentItem>
             ))}
