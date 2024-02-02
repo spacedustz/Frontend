a6a333f829c451ea12208f96b9b3c1cd4998fdb6
spacedustz <spacedustw@gmail.com>
2024-02-02 16:30:42 +0900

    Main Tab Style 분리

---
+++ React/src/styles/tabs/TabStyle.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import Tabs from "react-bootstrap/Tabs";
+
+export const TabsStyle = styled(Tabs)`
+    overflow: hidden;
+    background-color: lightblue;
+    
+    button {
+        color: black;
+
+        &:hover {
+            background-color: dimgray;
+            color: black;
+            transition: color 0.5s;
+        }
+    }
+    
+    @media (max-width: 768px) {
+        .tab-pane {
+            min-height: 100vh;
+        }
+        
+        span {
+            font-size: 12px;
+        }
+    }
+`;
