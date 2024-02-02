f653cab5307506061cde8fa1aca03ee46fb7ea5c
owen <wavefunctionc@gmail.com>
2024-02-02 16:34:52 +0900

    Pagination 스타일 코드 분리

--- React/src/components/Comment/Pagination.tsx
+++ React/src/components/Comment/Pagination.tsx
@@ -1,32 +1,6 @@
 import React from "react";
 import {PaginationProps} from "../../model/Comment.ts";
-import styled from "styled-components";
-
-const PaginationContainer = styled.div`
-    ul {
-        list-style: none;
-        padding: 0;
-        display: flex;
-        justify-content: center;
-        margin-top: 20px;
-    }
-
-    li {
-        margin-right: 10px;
-    }
-
-    button {
-        background-color: #3498db;
-        color: #fff;
-        border: 1px solid #3498db;
-        padding: 8px 12px;
-        cursor: pointer;
-
-        &:hover {
-            background-color: #217dbb;
-        }
-    }
-`;
+import {PaginationContainer} from "../../styles/comment/Pagination.ts";
 
 const Pagination: React.FC<PaginationProps> = ({itemsPerPage, totalItems, paginate }) => {
     const pageNumbers = [];
