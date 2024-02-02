954fb33d00998d4edb6bbe5330d14641012fb45d
spacedustz <spacedustw@gmail.com>
2024-02-02 16:34:17 +0900

    CommentForm 스타일 코드 분리

--- React/src/components/Comment/CommentForm.tsx
+++ React/src/components/Comment/CommentForm.tsx
@@ -1,37 +1,6 @@
 import React, {useState} from 'react';
-import styled from "styled-components";
 import {postComment} from "../../model/Api.ts";
-
-const InputStyle = styled.input`
-    border-radius: 5px;
-    width: 200px;
-    height: 22px;
-    border: 1px solid #dfe1e5;
-    text-align: center;
-    font-size: 12px;
-`;
-
-const ButtonStyle = styled.button`
-    border: 1px solid;
-    border-radius: 4px;
-    font-family: Apple SD Gothic Neo,arial,sans-serif;
-    font-size: 14px;
-    margin: 11px 4px;
-    padding: 0 16px;
-    line-height: 27px;
-    height: 32px;
-    min-width: 54px;
-    text-align: center;
-    cursor: pointer;
-    user-select: none;
-
-    &:hover {
-        box-shadow: 0 1px 1px rgba(0,0,0,.1);
-        background-color: #f8f9fa;
-        border: 1px solid #dadce0;
-        color: #202124;
-    }
-`;
+import {ButtonStyle, CommentFormContainer, InputStyle} from "../../styles/comment/Form.ts";
 
 const CommentForm: React.FC = () => {
     const [newComment, setNewComment] = useState<string>('');
@@ -57,16 +26,16 @@ const CommentForm: React.FC = () => {
     };
 
     return (
-        <div>
-            <h3>🐣 피드백 추가🐣 </h3>
+        <CommentFormContainer>
+            <h3>🐣 Guest Book 🐣 </h3>
             <InputStyle
                 type="text"
                 value={newComment}
                 onChange={handleInputChange}
-                placeholder="피드백을 입력해주세요!!"
+                placeholder="여러분의 소중한 댓글을 입력해주세요."
             />
             <ButtonStyle onClick={handleAddComment}>추가</ButtonStyle>
-        </div>
+        </CommentFormContainer>
     );
 };
 
