5d765dea280231139727ae7c99e94114172bdc62
owen <wavefunctionc@gmail.com>
2024-02-02 21:24:26 +0900

    Main App - Auth Provider 추가

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -3,15 +3,18 @@ import {RootContainer} from "./styles/container/RootContainer.ts";
 import Navigation from "./components/Navigation.tsx";
 import Router from "./router/Router.tsx";
 import ProfileCard from "./components/ProfileCard.tsx";
+import {AuthProvider} from "./components/auth/AutnContext.tsx";
 
 const App: React.FC = () => {
-  return (
-    <RootContainer>
-      <Navigation />
-        <ProfileCard />
-        <Router />
-    </RootContainer>
-  )
+    return (
+        <AuthProvider>
+            <RootContainer>
+                <Navigation/>
+                <ProfileCard/>
+                <Router/>
+            </RootContainer>
+        </AuthProvider>
+    )
 }
 
 export default App;
