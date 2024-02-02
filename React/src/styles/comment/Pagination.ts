45cf25c153f16b9bc9cece76200515f5c36f4251
spacedustz <spacedustw@gmail.com>
2024-02-02 16:32:11 +0900

    Pagination 스타일 분리

---
+++ React/src/styles/comment/Pagination.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const PaginationContainer = styled.div`
+    ul {
+        list-style: none;
+        padding: 0;
+        display: flex;
+        justify-content: center;
+        margin-top: 20px;
+    }
+
+    li {
+        margin-right: 10px;
+    }
+
+    button {
+        background-color: rgba(52, 152, 219, 0.1);
+        color: #fff;
+        border: 1px solid #3498db;
+        border-radius: 10px;
+        padding: 8px 12px;
+        cursor: pointer;
+
+        &:hover {
+            background-color: #217dbb;
+        }
+    }
+`;
