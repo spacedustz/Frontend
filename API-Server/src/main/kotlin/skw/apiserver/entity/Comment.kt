da64e6282de1c7810cef74e68894bbafc6bd4594
spacedustz <spacedustw@gmail.com>
2024-02-03 16:04:09 +0900

    Entity 생성 Time Zone 명시적 지정

--- API-Server/src/main/kotlin/skw/apiserver/entity/Comment.kt
+++ API-Server/src/main/kotlin/skw/apiserver/entity/Comment.kt
@@ -7,6 +7,7 @@ import jakarta.persistence.Id
 import jakarta.persistence.ManyToOne
 import lombok.NoArgsConstructor
 import java.time.LocalDateTime
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
 @Entity
@@ -26,7 +27,7 @@ data class Comment(
     companion object {
         fun createOf(description: String, user: User): Comment {
             val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH시 mm분 ss초")
-            return Comment(description, LocalDateTime.now().format(formatter).toString(), user)
+            return Comment(description, LocalDateTime.now().atZone(ZoneId.of("Asia/Seoul")).format(formatter).toString(), user)
         }
     }
 }
