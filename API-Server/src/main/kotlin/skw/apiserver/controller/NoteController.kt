6f63308d6c301a718bfb986e8c2989c78c1008df
owen <wavefunctionc@gmail.com>
2024-02-04 01:32:27 +0900

    Note Controller 추가

---
+++ API-Server/src/main/kotlin/skw/apiserver/controller/NoteController.kt
@@ -0,0 +1,30 @@
+package skw.apiserver.controller
+
+import lombok.RequiredArgsConstructor
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import skw.apiserver.dto.PostNoteDto
+import skw.apiserver.entity.Note
+import skw.apiserver.service.NoteService
+
+@RestController
+@RequestMapping("/api/note")
+@RequiredArgsConstructor
+class NoteController(
+    private val noteService: NoteService
+) {
+    @PostMapping
+    fun postNote(@RequestBody data: PostNoteDto): ResponseEntity<Unit> {
+        return ResponseEntity(noteService.postNote(data), HttpStatus.CREATED)
+    }
+
+    @GetMapping
+    fun getAllNote(): ResponseEntity<List<Note>> {
+        return ResponseEntity(noteService.getAllNote(), HttpStatus.OK)
+    }
+}
