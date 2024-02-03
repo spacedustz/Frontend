10f33ae168004daa6a197883dc6a8bdcd79728e4
spacedustz <tensorflow555@gmail.com>
2024-02-04 01:33:02 +0900

    Navigation에 Home 버튼 추가

--- React/src/components/Navigation.tsx
+++ React/src/components/Navigation.tsx
@@ -2,14 +2,27 @@ import Nav from 'react-bootstrap/Nav';
 import React from "react";
 import SignIn from "./auth/SignIn.tsx";
 import {NavImageStyle, NavSignInStyle, NavStyle} from "../styles/container/NavigatinContainer.ts";
+import {useNavigate} from "react-router-dom";
 
 const Navigation: React.FC = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/')
+    }
+
     return (
         <NavStyle bg="dark" data-bs-theme="dark">
             <Nav className="me-auto">
+                <Nav.Link onClick={handleClick}>
+                    <NavImageStyle src="../public/assets/home.png" alt="github" />
+                    Home
+                </Nav.Link>
+
                 <Nav.Link href="https://github.com/spacedustz" target="_blank">
                     <NavImageStyle src="../public/assets/profile/github.svg" alt="github"/>
                     Github</Nav.Link>
+
                 <Nav.Link href="https://iizz.tistory.com" target="_blank">
                     <NavImageStyle src="../public/assets/profile/blog.svg" alt="blog"/>
                     Blog</Nav.Link>
