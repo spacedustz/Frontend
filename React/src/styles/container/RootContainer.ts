a511b685f5f0734c3e65bfe399a76e89df693676
spacedustz <spacedustw@gmail.com>
2024-02-04 04:42:42 +0900

    Root Container Background 고정

--- React/src/styles/container/RootContainer.ts
+++ React/src/styles/container/RootContainer.ts
@@ -7,6 +7,7 @@ export const RootContainer = styled.div`
     background-image: url("../../../public/assets/background/night.jpg");
     background-size: cover;
     background-position: center center;
+    background-attachment: fixed;
     min-height: 100vh;
     width: 100vw;
     text-align: center;
