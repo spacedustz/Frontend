366c08089d3c78a883d414e131aa5d01b1a054a7
spacedustz <tensorflow555@gmail.com>
2024-02-04 01:31:22 +0900

    Main App 컴포넌트들 Router로 이동

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import {RootContainer} from "./styles/container/RootContainer.ts";
-import Navigation from "./components/Navigation.tsx";
 import Router from "./router/Router.tsx";
-import ProfileCard from "./components/ProfileCard.tsx";
 import {AuthProvider} from "./components/auth/AutnContext.tsx";
 
 const App: React.FC = () => {
     return (
         <AuthProvider>
             <RootContainer>
-                <Navigation/>
-                <ProfileCard/>
                 <Router/>
             </RootContainer>
         </AuthProvider>
