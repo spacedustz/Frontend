5cd373b73cbf9d4d6df1f81b6a5e89afd776681f
owen <wavefunctionc@gmail.com>
2024-02-04 15:23:31 +0900

    댓글 입력 화면 스타일 수정

--- React/src/components/Comment/CommentForm.tsx
+++ React/src/components/Comment/CommentForm.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {postComment} from "../../model/Api.ts";
-import {ButtonStyle, CommentFormContainer, FormHeader, InputStyle} from "../../styles/comment/Form.ts";
+import {ButtonStyle, CommentFormContainer, FormHeader, InputStyle, SubContainer} from "../../styles/comment/Form.ts";
 import {CommentFormProps} from "../../model/Comment.ts";
 
-const CommentForm: React.FC<CommentFormProps> = ({ newComment, onInputChange }) => {
+const CommentForm: React.FC<CommentFormProps> = ({newComment, onInputChange}) => {
     const handleAddComment = async () => {
         if (newComment.trim() !== '') {
             try {
                 await postComment(newComment);
-                onInputChange({ target: { value: '' } } as React.ChangeEvent<HTMLInputElement>);
+                onInputChange({target: {value: ''}} as React.ChangeEvent<HTMLInputElement>);
 
                 const name = sessionStorage.getItem('username')
                 console.log(`${name}님 댓글 작성 완료 - 작성 내용 : ${newComment}`)
@@ -24,14 +24,14 @@ const CommentForm: React.FC<CommentFormProps> = ({ newComment, onInputChange }) 
     return (
         <CommentFormContainer>
             <FormHeader>🐣 Guest Book 🐣 </FormHeader>
-            <InputStyle
-                type="text"
-                value={newComment}
-                onChange={onInputChange}
-                placeholder="여러분의 소중한 댓글을 입력해주세요."
-                maxLength={120}
-            />
-            <ButtonStyle onClick={handleAddComment}>Send</ButtonStyle>
+                <InputStyle
+                    type="text"
+                    value={newComment}
+                    onChange={onInputChange}
+                    placeholder="여러분의 소중한 댓글을 입력해주세요."
+                    maxLength={120}
+                />
+                <ButtonStyle onClick={handleAddComment}>Send</ButtonStyle>
         </CommentFormContainer>
     );
 };
