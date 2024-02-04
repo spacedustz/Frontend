27c77efc44d0fb60b1d7a452de9dc8742a482f5b
spacedustz <tensorflow555@gmail.com>
2024-02-04 20:38:19 +0900

    Markdown Writer 로직에서 스타일 코드 분리

---
+++ React/src/styles/note/Write.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 10px;
+`;
+
+export const TitleInput = styled.input`
+    margin: 0 10px;
+`;
+
+export const CategorySelect = styled.select`
+    margin-right: auto;
+`;
+
+export const SubmitButton = styled.button`
+    margin-left: auto;
+`;
+
+export const ContentTextarea = styled.textarea`
+    display: block;
+    width: 100%;
+    height: 200px;
+    margin: 0 auto;
+`;
