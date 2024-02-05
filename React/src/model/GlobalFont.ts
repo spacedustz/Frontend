7ff9de30fa31942b8e92d95f7074b0a31fb9103a
owen <wavefunctionc@gmail.com>
2024-02-06 05:37:11 +0900

    Global Font 추가 후 주석처리

---
+++ React/src/model/GlobalFont.ts
@@ -0,0 +1,11 @@
+// import {createGlobalStyle} from "styled-components";
+
+// export const GlobalFont = createGlobalStyle`
+//     @font-face {
+//         font-family: 'Dongle';
+//         src: url(../../public/assets/fonts/Dongle-Bold.ttf); format('truetype');
+//         font-weight: 600;
+//         font-style: normal;
+//         font-display: swap;
+//     }
+// `;
