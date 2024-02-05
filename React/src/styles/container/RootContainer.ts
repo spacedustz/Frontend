2178b1f2d291e9943d6bff96be69b49f840672f6
owen <wavefunctionc@gmail.com>
2024-02-06 05:37:25 +0900

    모든 컴포넌트 폰트 통일

--- React/src/styles/container/RootContainer.ts
+++ React/src/styles/container/RootContainer.ts
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const RootContainer = styled.div`
-    font-family: "Pretendard Variable",Pretendard,-apple-system,BlinkMacSystemFont,system-ui,Roboto,"Helvetica Neue","Segoe UI","Apple SD Gothic Neo","Noto Sans KR","Malgun Gothic","Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol",sans-serif;
     font-size: 15px;
     font-weight: bold;
     background-image: url("../../../public/assets/background/night.jpg");
