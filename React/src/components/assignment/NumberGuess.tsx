45e0f6bed67e022276827ac423665ddeef8f1ce7
owen <wavefunctionc@gmail.com>
2024-02-08 12:03:58 +0900

    Number Guess Game 컴포넌트 추가

---
+++ React/src/components/assignment/NumberGuess.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NumberGuess: React.FC = () => {
+
+    return (
+        <>
+            <h4>열심히 만드는중 ...</h4>
+        </>
+    );
+};
+
+export default NumberGuess;
