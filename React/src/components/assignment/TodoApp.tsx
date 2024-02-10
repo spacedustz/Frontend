140ff01ce41ba054cdfa89e051e555c596694119
owen <wavefunctionc@gmail.com>
2024-02-10 22:20:55 +0900

    TodoApp 아이템 삭제,수정,완료 추가

--- React/src/components/assignment/TodoApp.tsx
+++ React/src/components/assignment/TodoApp.tsx
@@ -5,29 +5,56 @@ import MarkdownComponent from "../note/MarkdownComponent.tsx";
 import rehypeRaw from "rehype-raw";
 import rehypeSanitize from "rehype-sanitize";
 import gfm from "remark-gfm";
-import {TodoContainer,
+import {
+    TodoContainer,
     StyledInput,
     StyledButton,
     HeaderSection,
     HeaderTab,
     Tab,
     Tasks,
-    UnderLine
+    UnderLine, DoneTasks
 } from "../../styles/assignment/TodoApp.ts";
+
+interface Task {
+    id: string
+    content: string
+    isDone: boolean
+}
 
 const TodoApp: React.FC = () => {
     const [input, setInput] = useState<string>('');
-    const [taskList, setTaskList] = useState<string[]>([]);
+    const [taskList, setTaskList] = useState<Task[]>([]);
 
-    const handleButton = () => {
-        setTaskList([...taskList, input]);
+    const generateRandomId = () => {
+        return '_' + Math.random().toString(36).substring(2, 9);
+    }
+
+    const addTask = () => {
+        const task = {
+            id: generateRandomId(),
+            content: input,
+            isDone: false
+        }
+
+        setTaskList([...taskList, task]);
         setInput('');
     }
 
+    const toggleDone = (id: string) => {
+        setTaskList(taskList.map(task =>
+            task.id === id ? {...task, isDone: !task.isDone} : task
+        ));
+    }
+
+    const deleteTask = (id: string) => {
+        setTaskList(taskList.filter(task => task.id !== id));
+    }
+
     return (
-        <div>
+        <div style={{all: "initial"}}>
             <TodoContainer>
-                <h4>Todo List</h4>
+                <h4 style={{marginTop: "30px"}}>Todo List</h4>
 
                 <section>
                     <StyledInput
@@ -36,51 +63,44 @@ const TodoApp: React.FC = () => {
                         onChange={(e) => setInput(e.target.value)}
                         placeholder="할일을 입력 해주세요."
                     />
-                    <StyledButton onClick={handleButton}>+</StyledButton>
+                    <StyledButton onClick={addTask}>+</StyledButton>
                 </section>
 
                 <HeaderSection>
                     <HeaderTab>
-                        <UnderLine>
-
-                        </UnderLine>
-
-                        <Tab>
-                            All
-                        </Tab>
-
-                        <Tab>
-                            Not Done
-                        </Tab>
-
-                        <Tab>
-                            Done
-                        </Tab>
+                        <UnderLine/>
+                        <Tab>전체</Tab>
+                        <Tab>진행중</Tab>
+                        <Tab>완료</Tab>
                     </HeaderTab>
 
                     <div>
-                        {taskList.map((task: string) => (
-                            <Tasks>
-                                {task}
+                        {taskList.map((task) => (
+                            <Tasks key={task.id}>
+                                {task.isDone ? (
+                                    <DoneTasks>
+                                        <div style={{textDecoration: "line-through"}}>{task.content}</div>
 
-                                <div>
-                                    <StyledButton>Check</StyledButton>
+                                        <div>
+                                            <StyledButton onClick={() => toggleDone(task.id)}>취소</StyledButton>
+                                            <StyledButton onClick={() => deleteTask(task.id)}>Delete</StyledButton>
+                                        </div>
+                                    </DoneTasks>
+                                ) : (
+                                    <Tasks>
+                                        <div>{task.content}</div>
 
-                                    <StyledButton>Delete</StyledButton>
-                                </div>
+                                        <div>
+                                            <StyledButton onClick={() => toggleDone(task.id)}>완료</StyledButton>
+                                            <StyledButton onClick={() => deleteTask(task.id)}>Delete</StyledButton>
+                                        </div>
+                                    </Tasks>
+                                )}
                             </Tasks>
                         ))}
 
                     </div>
                 </HeaderSection>
-
-                <div>
-
-                </div>
-
-                <div>
-
-                </div>
             </TodoContainer>
 
             <div style={{all: 'initial'}}>
