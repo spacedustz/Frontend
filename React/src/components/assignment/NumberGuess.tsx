eca2d6b9c7aefbcb50e8f7454be005c1f35f3779
owen <wavefunctionc@gmail.com>
2024-02-10 15:05:17 +0900

    Number Guess App Style 분리

--- React/src/components/assignment/NumberGuess.tsx
+++ React/src/components/assignment/NumberGuess.tsx
@@ -5,55 +5,9 @@ import MarkdownComponent from "../note/MarkdownComponent.tsx";
 import rehypeRaw from "rehype-raw";
 import rehypeSanitize from "rehype-sanitize";
 import gfm from "remark-gfm";
-import styled from "styled-components";
 import ConfettiExplosion from "react-confetti-explosion";
 import {NumberGameNote} from "../../model/Assignment.ts";
-
-const GameContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    margin-top: 30px;
-`;
-
-const StyledInput = styled.input`
-    margin: 10px;
-    padding: 5px;
-    font-size: 16px;
-
-    &:focus {
-        outline: none;
-        border: 1px solid orange;
-        box-shadow: 0 0 5px orange;
-    }
-
-    &::placeholder {
-        color: rgba(30, 30, 30, 0.7);
-        font-weight: 300;
-        font-size: 15px;
-        padding-left: 10px;
-    }
-`;
-
-const StyledButton = styled.button`
-    padding: 5px 10px;
-    font-size: 16px;
-    background-color: #4CAF50; /* Green */
-    border: none;
-    color: white;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    margin: 4px 2px;
-    transition-duration: 0.4s;
-    cursor: pointer;
-
-    &:disabled {
-        background-color: #cccccc;
-        cursor: not-allowed;
-    }
-`;
+import {GameContainer, StyledButton, StyledInput} from "../../styles/assignment/NumberGuess.ts";
 
 /**
  * TODO
